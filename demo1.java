ab37d37b2a92eb73f8d428880fda6d296d5d23ba
Mohana Kavya K <kkunisettymohana@student.nitw.ac.in>
2020-10-06 17:10:29 +0530

    UC1 1 commmit

--- demo1.java
+++ demo1.java
@@ -1,10 +1,12 @@
 package demo;
 
+import java.util.Map;
+
 public class demo1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("UC1");
 	}
 
 }
